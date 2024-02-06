5c43e01e5d70559a1f49b0cea2803df13b00a514
Ewan Jee <830jiforcareer@gmail.com>
2024-02-05 23:23:21 -0500

    feat : application 개발

---
+++ butpal/src/main/kotlin/bukpal/application/port/in/SendMoneyUseCase.kt
@@ -0,0 +1,7 @@
+package bukpal.application.port.`in`
+
+interface SendMoneyUseCase{
+    /**
+     * 메서드 패러미터에서 Account, Activity 이용?
+     */
+}
---
+++ butpal/src/main/kotlin/bukpal/application/port/out/UpdateAccountStatePort.kt
@@ -0,0 +1,4 @@
+package bukpal.application.port.out
+
+interface UpdateAccountStatePort {
+}
